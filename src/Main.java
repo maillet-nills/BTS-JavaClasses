dcfc51d4c777e6f3f63e921821e6eac3ac8aa435
Nills Maillet <mailletnills@gmail.com>
2026-04-07 13:21:58 +0200

    done: Exercice 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
+    // Exercice 1
+    String marque= "Peugeot";
+    int modele = 208;
+    int annee = 2021;
+    int prix = 14500;
 
+    System.out.println("La voiture est une " + marque + " " + modele + " datant de " + annee + ", elle coûte " + prix + "€.");
 }
