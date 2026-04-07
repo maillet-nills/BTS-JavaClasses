a50fa087aab0855bcc725bf84a8788a841b0474d
Nills Maillet <mailletnills@gmail.com>
2026-04-07 13:34:49 +0200

    done: Exercice 2

--- src/Main.java
+++ src/Main.java
@@ -8,4 +8,11 @@ void main() {
     int prix = 14500;
 
     System.out.println("La voiture est une " + marque + " " + modele + " datant de " + annee + ", elle coûte " + prix + "€.");
+
+    // Exercice 2
+    // 1. Il faudrait à chaque fois créer des variables ce qui n'est pas pratique.
+    // 2. Créer une nouvelle variable couleur par exemple et/ou ne plus en utiliser une.
+    // 3. La création d'une nouvelle variable pour chaque voiture déjà existante.
+    // 4. Il faudra retrouver la variable et re-assigner sa valeur.
+    // 5. Cette méthode n'est pas optimale pour Auto+ car il gère plus de 200 véhicules différents.
 }
