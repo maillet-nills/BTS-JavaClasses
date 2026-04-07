5aeb155bf8226f357bb10ef62b1dca7140796ad2
Nills Maillet <mailletnills@gmail.com>
2026-04-07 13:52:55 +0200

    done: Exercice 4

--- src/Main.java
+++ src/Main.java
@@ -18,7 +18,44 @@ void main() {
 
     // Exercice 3
     // 1. Ce nouveau code nous permet d'avoir une structure préfaite de ce "qu'est une voiture" pour Auto+.
-    // 2. Ce nouveau code nous permet de décrire une voiture car elle contient toutes les "variables" nécessaires.
-    // 3. Une classe est une "forme" contenant des propriétés. Grâce aux classes nous pouvons créer des "Objets", instancces de ces classes.
+    // 2. Ce nouveau code nous permet de décrire une voiture, car elle contient toutes les "variables" nécessaires.
+    // 3. Une classe est une "forme" contenant des propriétés. Grâce aux classes, nous pouvons créer des "Objets", instancces de ces classes.
+
+    //Exercice 4
+    //Voiture v1 = new Voiture();
+    //Voiture v2 = new Voiture();
+
+    // 1. v1 et v2 représente chacun une voiture.
+    // 2. Nous pouvons créer plusieurs variables de type voiture puisqu'il s'agit de voitures différentes et que "Voiture" est une classe.
+    // 3. Actuellement, nous ne connaissons pas le modèle ou la marque des deux voitures.
+    // 4.
+
+    //v1.marque = "Peugeot";
+    //v2.marque = "Toyota";
+
+    // 5. Un objet est une instance d'une classe. Plusieurs objets d'une classe peuvent exister.
+
+    // 6. Mis à jour du code.
+
+    Voiture v1 = new Voiture();
+    Voiture v2 = new Voiture();
+    Voiture v3 = new Voiture();
+
+    v1.marque = "Peugeot";
+    v1.modele = "208";
+    v1.annee = 2021;
+    v1.prix = 14500;
+
+    v2.marque = "Renault";
+    v2.modele = "Clio";
+    v2.annee = 2000;;
+    v2.prix = 3500;
+
+    v3.marque = "Toyota";
+    v3.modele = "Yaris";
+    v3.annee = 2015;
+    v3.prix = 9800;
+
+    // 7. Plus besoin de créer des variables différentes, en revanche cela prend toujours beaucoup de lignes, ce n'est pas efficace.
 
 }
